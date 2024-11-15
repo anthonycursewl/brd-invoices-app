35810d4ab65cbb08270f9b45831a6b73c00e127c
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-11-15 15:54:42 -0400

    @Breadriuss: fixed modals

--- src/Dashboard/Dash-sections/dash-sections.tsx
+++ src/Dashboard/Dash-sections/dash-sections.tsx
@@ -8,8 +8,6 @@ export default function DashSections() {
     <div className="dash-sections">
         <DashCardSection text="Registra facturas y genera facturas para ser compartidas con un link para terceros." title="Facturas" children={<InvoiceIcon/>} path="/dashboard/payments/add"/>
         <DashCardSection text="Registra pagos que hayan sido efectuados. Lleva el control de los pagos de tus facturas." title="Pagos" children={<NewPayment/>} path="/dashboard/invoices/add"/>
-        <DashCardSection text="Registra facturas y genera facturas para ser compartidas con un link para terceros." title="Facturas" children={<InvoiceIcon/>} path="/dashboard/payments/add"/>
-        <DashCardSection text="Registra pagos que hayan sido efectuados. Lleva el control de los pagos de tus facturas." title="Pagos" children={<NewPayment/>} path="/dashboard/invoices/add"/>
     </div>
   );
 }
