8a933d8fe76f33d979692200b979eb434b9caf76
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-12-07 19:25:15 -0400

    Added: Sessions | Config | Repositories

--- src/Config/tajinside.config.ts
+++ src/Config/tajinside.config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:4200';
+export const API_URL = 'https://api-brd-invoices-production.up.railway.app';
 
 // Fecha para usar en la app
 export const brd_Date = new Date()
